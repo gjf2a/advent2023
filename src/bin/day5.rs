36d15a53fd29323c2aa174898b615d021570ce91
gjf2a <ferrer@hendrix.edu>
2023-12-05 06:20:15 -0600

    Part 1 works!

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -10,15 +10,13 @@ fn main() -> anyhow::Result<()> {
                 let mut lines = all_lines(filename)?;
                 let mut seeds = get_seeds(lines.next().unwrap());
                 lines.next().unwrap();
+                let num_seeds = seeds.len();
+                println!("{num_seeds} seeds");
                 let mut mapped_seeds = IndexSet::new();
                 while let Some(line) = lines.next() {
                     match line.chars().next() {
                         None => {
-                            for seed in seeds.drain(..) {
-                                mapped_seeds.insert(seed);
-                            }
-                            std::mem::swap(&mut seeds, &mut mapped_seeds);
-                            mapped_seeds = IndexSet::new();
+                            finish_mapping(&mut seeds, &mut mapped_seeds);
                         }
                         Some(c) => match c {
                             '0'..='9' => {line.parse::<Mapping>()?.remap(&mut seeds, &mut mapped_seeds);}
@@ -27,6 +25,7 @@ fn main() -> anyhow::Result<()> {
                         }
                     }
                 }
+                finish_mapping(&mut seeds, &mut mapped_seeds);
                 let part1 = seeds.iter().min().copied().unwrap();
                 println!("Part 1: {part1}");
             }
@@ -42,6 +41,14 @@ fn get_seeds(line: String) -> IndexSet<u64> {
     line.split_whitespace().skip(1).map(|n| n.parse::<u64>().unwrap()).collect()
 }
 
+fn finish_mapping(seeds: &mut IndexSet<u64>, mapped_seeds: &mut IndexSet<u64>) {
+    for seed in seeds.drain(..) {
+        mapped_seeds.insert(seed);
+    }
+    std::mem::swap(seeds, mapped_seeds);
+    *mapped_seeds = IndexSet::new();
+}
+
 struct Mapping {
     destination: u64,
     source: u64,
@@ -50,11 +57,13 @@ struct Mapping {
 
 impl Mapping {
     fn remap(&self, prev: &mut IndexSet<u64>, next: &mut IndexSet<u64>) {
+        let start_count = prev.len() + next.len();
         let mappings = prev.iter().filter_map(|n| self.mapping(*n).map(|m| (*n, m))).collect::<Vec<_>>();
         for (prev_num, next_num) in mappings {
             prev.remove(&prev_num);
             next.insert(next_num);
         }
+        assert_eq!(start_count, prev.len() + next.len());
     }
 
     fn mapping(&self, value: u64) -> Option<u64> {
