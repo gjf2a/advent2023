6b4582011c9a04471f2a0bb6c24c0d18b5b8855f
gjf2a <ferrer@hendrix.edu>
2023-12-08 08:09:40 -0600

    Part 2 works!

--- src/bin/day8.rs
+++ src/bin/day8.rs
@@ -1,5 +1,5 @@
-use advent_code_lib::{chooser_main, Part, all_lines};
-use bare_metal_modulo::{ModNum, MNum};
+use advent_code_lib::{all_lines, chooser_main, Part};
+use bare_metal_modulo::{MNum, ModNum};
 use indexmap::IndexMap;
 
 fn main() -> anyhow::Result<()> {
@@ -15,34 +15,39 @@ fn main() -> anyhow::Result<()> {
     })
 }
 
-fn navigate(start: &str, instructions: &Vec<char>, map: &IndexMap<String,(String,String)>) -> u128 {
+fn navigate(
+    start: &str,
+    instructions: &Vec<char>,
+    map: &IndexMap<String, (String, String)>,
+) -> u64 {
     let mut step_count = 0;
     let mut i = ModNum::new(0, instructions.len());
     let mut location = start.to_owned();
     while !location.as_str().ends_with("Z") {
         let options = map.get(location.as_str()).unwrap();
-        location = if instructions[i.a()] == 'L' {options.0.clone()} else {options.1.clone()};
+        location = if instructions[i.a()] == 'L' {
+            options.0.clone()
+        } else {
+            options.1.clone()
+        };
         i += 1;
         step_count += 1;
     }
     step_count
 }
 
-fn ghost_navigate(instructions: &Vec<char>, map: &IndexMap<String,(String,String)>) -> u128 {
+fn ghost_navigate(instructions: &Vec<char>, map: &IndexMap<String, (String, String)>) -> u64 {
     let locations = all_starts(map);
     let distances = all_distances(&locations, instructions, map);
-    let product = distances.iter().product::<u128>();
-    let gcd = big_gcd(&distances) * (distances.len() - 1) as u128;
-    let answer = product / gcd;
-    println!("distances: {distances:?} product: {product} gcd: {gcd} answer: {answer}");
-    answer
+    println!("distances: {distances:?}");
+    distances.iter().copied().reduce(lcm).unwrap()
 }
 
-fn big_gcd(ns: &Vec<u128>) -> u128 {
-    ns.iter().copied().reduce(gcd).unwrap()
+fn lcm(a: u64, b: u64) -> u64 {
+    a * b / gcd(a, b)
 }
 
-fn gcd(a: u128, b: u128) -> u128 {
+fn gcd(a: u64, b: u64) -> u64 {
     if a == 0 {
         b
     } else if b == 0 {
@@ -52,24 +57,37 @@ fn gcd(a: u128, b: u128) -> u128 {
     }
 }
 
-fn all_starts(map: &IndexMap<String,(String,String)>) -> Vec<String> {
-    map.keys().filter(|k| k.ends_with("A")).map(|k| k.to_string()).collect()
+fn all_starts(map: &IndexMap<String, (String, String)>) -> Vec<String> {
+    map.keys()
+        .filter(|k| k.ends_with("A"))
+        .map(|k| k.to_string())
+        .collect()
 }
 
-fn all_distances(locations: &Vec<String>, instructions: &Vec<char>, map: &IndexMap<String,(String,String)>) -> Vec<u128> {
-    locations.iter().map(|start| navigate(start.as_str(), instructions, map)).collect()
+fn all_distances(
+    locations: &Vec<String>,
+    instructions: &Vec<char>,
+    map: &IndexMap<String, (String, String)>,
+) -> Vec<u64> {
+    locations
+        .iter()
+        .map(|start| navigate(start.as_str(), instructions, map))
+        .collect()
 }
 
 fn instructions(line: String) -> Vec<char> {
     line.chars().collect()
 }
 
-fn graph(lines: impl Iterator<Item=String>) -> IndexMap<String,(String,String)> {
+fn graph(lines: impl Iterator<Item = String>) -> IndexMap<String, (String, String)> {
     let mut result = IndexMap::new();
     for line in lines {
         let line = line.replace('(', "").replace(')', "").replace(',', "");
         let parts: Vec<&str> = line.split_whitespace().collect();
-        result.insert(parts[0].to_owned(), (parts[2].to_owned(), parts[3].to_owned()));
+        result.insert(
+            parts[0].to_owned(),
+            (parts[2].to_owned(), parts[3].to_owned()),
+        );
     }
     result
 }
