21c809703903e9819093c14312e657b449ab5d58
gjf2a <ferrer@hendrix.edu>
2023-12-10 07:04:41 -0600

    Ran cargo fmt.

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -1,6 +1,6 @@
 use std::collections::VecDeque;
 
-use advent_code_lib::{chooser_main, Part, Position, ManhattanDir, all_lines, DirType};
+use advent_code_lib::{all_lines, chooser_main, DirType, ManhattanDir, Part, Position};
 use enum_iterator::all;
 use indexmap::IndexMap;
 
@@ -23,34 +23,58 @@ fn main() -> anyhow::Result<()> {
 
 #[derive(Debug)]
 struct PipeMaze {
-    pipes: IndexMap<Position,[ManhattanDir; 2]>,
+    pipes: IndexMap<Position, [ManhattanDir; 2]>,
     start: Position,
 }
 
 impl PipeMaze {
     fn from_file(filename: &str) -> anyhow::Result<Self> {
-        let mut pipes = IndexMap::new();
-        let mut start = Position::default();
+        let mut result = Self {
+            pipes: IndexMap::new(),
+            start: Position::new(),
+        };
         for (row, row_text) in all_lines(filename)?.enumerate() {
-            for (col, col_char) in row_text.char_indices() {
-                let p = Position {row: row as isize, col: col as isize};
-                match col_char {
-                    'S' => {start = p;}
-                    '|' => {pipes.insert(p, [ManhattanDir::N, ManhattanDir::S]);}
-                    '-' => {pipes.insert(p, [ManhattanDir::E, ManhattanDir::W]);}
-                    '7' => {pipes.insert(p, [ManhattanDir::W, ManhattanDir::S]);}
-                    'L' => {pipes.insert(p, [ManhattanDir::N, ManhattanDir::E]);}
-                    'F' => {pipes.insert(p, [ManhattanDir::S, ManhattanDir::E]);}
-                    'J' => {pipes.insert(p, [ManhattanDir::N, ManhattanDir::W]);}
-                    _ => {}
-                }
+            for (col, pipe) in row_text.char_indices() {
+                result.add_pipe(row, col, pipe);
             }
         }
-        let mut result = Self {pipes, start};
         let start_incoming = result.incoming(&result.start);
         assert_eq!(2, start_incoming.len());
-        result.pipes.insert(result.start, [start_incoming[0].0, start_incoming[1].0]);
+        result
+            .pipes
+            .insert(result.start, [start_incoming[0].0, start_incoming[1].0]);
         Ok(result)
+    }
+
+    fn add_pipe(&mut self, row: usize, col: usize, pipe: char) {
+        let p = Position {
+            row: row as isize,
+            col: col as isize,
+        };
+        match pipe {
+            'S' => {
+                self.start = p;
+            }
+            '|' => {
+                self.pipes.insert(p, [ManhattanDir::N, ManhattanDir::S]);
+            }
+            '-' => {
+                self.pipes.insert(p, [ManhattanDir::E, ManhattanDir::W]);
+            }
+            '7' => {
+                self.pipes.insert(p, [ManhattanDir::W, ManhattanDir::S]);
+            }
+            'L' => {
+                self.pipes.insert(p, [ManhattanDir::N, ManhattanDir::E]);
+            }
+            'F' => {
+                self.pipes.insert(p, [ManhattanDir::S, ManhattanDir::E]);
+            }
+            'J' => {
+                self.pipes.insert(p, [ManhattanDir::N, ManhattanDir::W]);
+            }
+            _ => {}
+        }
     }
 
     fn distance_map(&self) -> IndexMap<Position, u64> {
@@ -74,8 +98,8 @@ impl PipeMaze {
 
     fn incoming(&self, p: &Position) -> Vec<(ManhattanDir, Position)> {
         all::<ManhattanDir>()
-        .map(|d| (d, d.next_position(*p)))
-        .filter(|(_,n)| self.outgoing(n).map_or(false, |out| out.contains(p)))
-        .collect()
+            .map(|d| (d, d.next_position(*p)))
+            .filter(|(_, n)| self.outgoing(n).map_or(false, |out| out.contains(p)))
+            .collect()
     }
 }
