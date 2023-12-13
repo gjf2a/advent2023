82aecfbe000ad2b2144797d997f44e28272980f8
gjf2a <ferrer@hendrix.edu>
2023-12-13 14:56:18 -0600

    Removed duplicated code.

--- src/bin/day13.rs
+++ src/bin/day13.rs
@@ -69,24 +69,11 @@ fn find_smudge_rows(block: &GridCharWorld) -> Option<usize> {
 }
 
 fn num_columns_left(block: &GridCharWorld) -> Option<usize> {
-    Mirror::Column.num_preceding(block)/*
-    for col in 0..block.width() {
-        if mirror_col(block, col) {
-            return Some(col);
-        }
-    }
-    None*/
+    Mirror::Column.num_preceding(block)
 }
 
 fn num_rows_above(block: &GridCharWorld) -> Option<usize> {
-    Mirror::Row.num_preceding(block)/*
-    for row in 0..block.height() {
-        if mirror_row(block, row) {
-            return Some(row);
-        }
-    }
-    None
-    */
+    Mirror::Row.num_preceding(block)
 }
 
 #[derive(Copy, Clone, Eq, PartialEq)]
@@ -146,44 +133,6 @@ impl Mirror {
     }
 }
 
-fn mirror_col(block: &GridCharWorld, col: usize) -> bool {
-    let subwidth = min(col, block.width() - col);
-    if subwidth >= 1 {
-        let substart = col - subwidth;
-        let subend = (substart + subwidth) * 2 - 1;
-        for subcol in substart..col {
-            let mirror_col = subend - subcol;
-            for row in 0..block.height() {
-                if block.get(subcol, row) != block.get(mirror_col, row) {
-                    return false;
-                }
-            }
-        }
-        true
-    } else {
-        false
-    }
-}
-
-fn mirror_row(block: &GridCharWorld, row: usize) -> bool {
-    let subheight = min(row, block.height() - row);
-    if subheight >= 1 {
-        let substart = row - subheight;
-        let subend = (substart + subheight) * 2 - 1;
-        for subrow in substart..row {
-            let mirror_row = subend - subrow;
-            for col in 0..block.width() {
-                if block.get(col, subrow) != block.get(col, mirror_row) {
-                    return false;
-                }
-            }
-        }
-        true
-    } else {
-        false
-    }
-}
-
 fn blocks_from(filename: &str) -> anyhow::Result<Vec<GridCharWorld>> {
     let input = all_lines(filename)?
         .map(|line| format!("{line}\n"))
@@ -196,12 +145,12 @@ fn blocks_from(filename: &str) -> anyhow::Result<Vec<GridCharWorld>> {
 
 #[cfg(test)]
 mod tests {
-    use crate::{blocks_from, mirror_col, mirror_row, num_columns_left, num_rows_above};
+    use crate::{blocks_from, num_columns_left, num_rows_above, Mirror};
 
     #[test]
     fn test_horizontal() {
         let blocks = blocks_from("ex/day13.txt").unwrap();
-        assert!(mirror_col(&blocks[0], 5));
+        assert!(Mirror::Column.mirror(&blocks[0], 5));
         assert_eq!(Some(5), num_columns_left(&blocks[0]));
         assert_eq!(None, num_columns_left(&blocks[1]));
     }
@@ -209,7 +158,7 @@ mod tests {
     #[test]
     fn test_second_horizontal() {
         let blocks = blocks_from("ex/day13ferrer.txt").unwrap();
-        assert!(mirror_col(&blocks[0], 6));
+        assert!(Mirror::Column.mirror(&blocks[0], 6));
         assert_eq!(Some(6), num_columns_left(&blocks[0]));
         assert_eq!(None, num_columns_left(&blocks[1]));
     }
@@ -217,10 +166,10 @@ mod tests {
     #[test]
     fn test_input_horizontal() {
         let blocks = blocks_from("ex/day13_input_instances.txt").unwrap();
-        assert!(mirror_col(&blocks[0], 4));
+        assert!(Mirror::Column.mirror(&blocks[0], 4));
         assert_eq!(Some(4), num_columns_left(&blocks[0]));
 
-        assert!(mirror_row(&blocks[1], 14));
+        assert!(Mirror::Row.mirror(&blocks[1], 14));
         assert_eq!(Some(14), num_rows_above(&blocks[1]));
     }
 }
