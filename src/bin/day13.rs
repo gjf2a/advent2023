c5ce1a95b8383abe61f1c7847940ab17dae906b1
gjf2a <ferrer@hendrix.edu>
2023-12-13 09:06:37 -0600

    Part 1 still does not work!

--- src/bin/day13.rs
+++ src/bin/day13.rs
@@ -3,69 +3,84 @@ use std::cmp::min;
 use advent_code_lib::{chooser_main, all_lines, GridCharWorld};
 
 // Part 1: 551 is too low
-// 769 is also too low - I got there by starting with higher columns and rows and going down.
+// 21358 is still too low!!
 
 fn main() -> anyhow::Result<()> {
     chooser_main(|filename, part| {
         let blocks = blocks_from(filename)?;
-        let left_sum = blocks.iter().filter_map(|b| num_columns_left(b)).sum::<usize>();
-        let above_sum = blocks.iter().filter_map(|b| num_rows_above(b)).sum::<usize>();
+        let horizontal_blocks = blocks.iter().filter_map(|b| num_columns_left(b)).collect::<Vec<_>>();
+        let vertical_blocks = blocks.iter().filter_map(|b| num_rows_above(b)).collect::<Vec<_>>();
+        let left_sum = horizontal_blocks.iter().sum::<usize>();
+        let above_sum = vertical_blocks.iter().sum::<usize>();
         println!("num blocks: {}", blocks.len());
-        println!("left_sum: {left_sum}");
-        println!("above_sum: {above_sum}");
+        println!("left_sum: {left_sum} ({} blocks)", horizontal_blocks.len());
+        println!("above_sum: {above_sum} ({} blocks)", vertical_blocks.len());
         println!("Part 1: {}", above_sum * 100 + left_sum);
         Ok(())
     })
 }
 
 fn num_columns_left(block: &GridCharWorld) -> Option<usize> {
-    for col in (0..block.width()).rev() {
+    for col in 0..block.width() {
         if mirror_col(block, col) {
-            println!("{block}\n");
+            println!("{block}\ncol: {col}\n");
             return Some(col);
         }        
     }
+    println!("{block}\nH failure\n");
     None
 }
 
 fn num_rows_above(block: &GridCharWorld) -> Option<usize> {
-    for row in (0..block.height()).rev() {
+    for row in 0..block.height() {
         if mirror_row(block, row) {
-            println!("{block}\n");
+            println!("{block}\nrow: {row}\n");
             return Some(row);
         }
     }
+    println!("{block}\nV failure\n");
     None
 }
 
 fn mirror_col(block: &GridCharWorld, col: usize) -> bool {
     let subwidth = min(col, block.width() - col);
-    let substart = col - subwidth;
-    let subend = substart + subwidth * 2;
-    for subcol in substart..col {
-        let mirror_col = subend - subcol;
-        for row in 0..block.height() {
-            if block.get(subcol, row) != block.get(mirror_col, row) {
-                return false;
+    if subwidth >= 2 {
+        let substart = col - subwidth;
+        let subend = (substart + subwidth) * 2 - 1;
+        //println!("col: {col} sw: {subwidth} ss: {substart} se: {subend}");
+        for subcol in substart..col {
+            let mirror_col = subend - subcol;
+            //println!("{subcol} {mirror_col}");
+            for row in 0..block.height() {
+                if block.get(subcol, row) != block.get(mirror_col, row) {
+                    //println!("Fatal row: {row} {:?} {:?}", block.get(subcol, row), block.get(mirror_col, row));
+                    return false;
+                }
             }
         }
+        true
+    } else {
+        false
     }
-    subwidth > 1
 }
 
 fn mirror_row(block: &GridCharWorld, row: usize) -> bool {
     let subheight = min(row, block.height() - row);
-    let substart = row - subheight;
-    let subend = substart + subheight * 2;
-    for subrow in substart..row {
-        let mirror_row = subend - subrow;
-        for col in 0..block.width() {
-            if block.get(col, subrow) != block.get(col, mirror_row) {
-                return false;
+    if subheight >= 2 {
+        let substart = row - subheight;
+        let subend = (substart + subheight) * 2 - 1;
+        for subrow in substart..row {
+            let mirror_row = subend - subrow;
+            for col in 0..block.width() {
+                if block.get(col, subrow) != block.get(col, mirror_row) {
+                    return false;
+                }
             }
         }
+        true
+    } else {
+        false
     }
-    subheight > 1
 }
 
 fn blocks_from(filename: &str) -> anyhow::Result<Vec<GridCharWorld>> {
@@ -83,5 +98,20 @@ mod tests {
         assert!(mirror_col(&blocks[0], 5));
         assert_eq!(Some(5), num_columns_left(&blocks[0]));
         assert_eq!(None, num_columns_left(&blocks[1]));
+    }
+
+    #[test]
+    fn test_second_horizontal() {
+        let blocks = blocks_from("ex/day13ferrer.txt").unwrap();
+        assert!(mirror_col(&blocks[0], 6));
+        assert_eq!(Some(6), num_columns_left(&blocks[0]));
+        assert_eq!(None, num_columns_left(&blocks[1]));
+    }
+
+    #[test]
+    fn test_input_horizontal() {
+        let blocks = blocks_from("ex/day13_input_instances.txt").unwrap();
+        assert!(mirror_col(&blocks[0], 4));
+        assert_eq!(Some(4), num_columns_left(&blocks[0]));
     }
 }
