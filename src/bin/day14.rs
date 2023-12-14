09889384521c9d08c52e76594c004be9213f5e95
gjf2a <ferrer@hendrix.edu>
2023-12-14 16:13:12 -0600

    Refactored to allow any direction for rolling the rocks.

--- src/bin/day14.rs
+++ src/bin/day14.rs
@@ -1,44 +1,58 @@
-use advent_code_lib::{
-    chooser_main, GridCharWorld, Position,
-};
+use advent_code_lib::{chooser_main, GridCharWorld, ManhattanDir, Part, Position, DirType};
 
 fn main() -> anyhow::Result<()> {
     chooser_main(|filename, part| {
         let mut rocks = GridCharWorld::from_char_file(filename)?;
-        roll_rocks(&mut rocks);
-        println!("{rocks}");
-        println!("Part {part:?}: {}", calculate_load(&rocks));
+        match part {
+            Part::One => {
+                roll_rocks(&mut rocks, ManhattanDir::N);
+                println!("Part {part:?}: {}", calculate_load(&rocks));
+            }
+            Part::Two => {}
+        }
+
         Ok(())
     })
 }
 
-fn calculate_load(rocks: &GridCharWorld) -> usize {
-    rocks.position_value_iter()
-    .filter(|(_, c)| **c == 'O')
-    .map(|(p, _)| rocks.width() - p.row as usize)
-    .sum()
+fn rock_positions(rocks: &GridCharWorld) -> impl Iterator<Item=Position> + '_ {
+    rocks.position_value_iter().filter(|(_, c)| **c == 'O').map(|(p,_)| *p)
 }
 
-fn roll_rocks(rocks: &mut GridCharWorld) {
-    for p in rocks.position_iter() {
-        if rocks.value(p).unwrap() == 'O' {
-            let destination = roll_north(&rocks, p);
-            rocks.swap(p, destination);
-        }
+fn calculate_load(rocks: &GridCharWorld) -> usize {
+    rock_positions(rocks).map(|p| rocks.width() - p.row as usize).sum()
+}
+
+fn roll_rocks(rocks: &mut GridCharWorld, dir: ManhattanDir) {
+    let sorted_rocks = find_sorted_rocks(rocks, dir);
+    for rock in sorted_rocks {
+        let destination = roll_rock(&rocks, rock, dir);
+        rocks.swap(rock, destination);
     }
 }
 
-fn roll_north(rocks: &GridCharWorld, p: Position) -> Position {
+fn find_sorted_rocks(rocks: &GridCharWorld, dir: ManhattanDir) -> Vec<Position> {
+    let mut result = rock_positions(rocks).collect::<Vec<_>>();
+    result.sort_by_key(|k| dir_key(k, dir));
+    result
+}
+
+fn dir_key(p: &Position, dir: ManhattanDir) -> isize {
+    match dir {
+        ManhattanDir::N => p.row,
+        ManhattanDir::E => -p.col,
+        ManhattanDir::S => -p.row,
+        ManhattanDir::W => p.col,
+    }
+}
+
+fn roll_rock(rocks: &GridCharWorld, p: Position, dir: ManhattanDir) -> Position {
     let mut prev = p;
-    for above in (0..p.row).rev() {
-        let candidate = Position {
-            col: p.col,
-            row: above,
-        };
-        if rocks.value(candidate).unwrap() != '.' {
+    loop {
+        let candidate = dir.next_position(prev);
+        if !rocks.in_bounds(candidate) || rocks.value(candidate).unwrap() != '.' {
             return prev;
         }
         prev = candidate;
     }
-    Position { col: p.col, row: 0 }
 }
