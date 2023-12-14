3d0dfbe02e52f0cd60959f347d3585e4f4dc23de
gjf2a <ferrer@hendrix.edu>
2023-12-14 15:23:44 -0600

    Rolls rocks uphill.

--- src/bin/day14.rs
+++ src/bin/day14.rs
@@ -1,11 +1,12 @@
 use advent_code_lib::{
-    chooser_main, GridCharWorld, ManhattanDir, Position, RowMajorPositionIterator,
+    chooser_main, GridCharWorld, Position,
 };
 
 fn main() -> anyhow::Result<()> {
     chooser_main(|filename, part| {
-        let rocks = GridCharWorld::from_char_file(filename)?;
-
+        let mut rocks = GridCharWorld::from_char_file(filename)?;
+        roll_rocks(&mut rocks);
+        println!("{rocks}");
         Ok(())
     })
 }
@@ -13,18 +14,23 @@ fn main() -> anyhow::Result<()> {
 fn roll_rocks(rocks: &mut GridCharWorld) {
     for p in rocks.position_iter() {
         if rocks.value(p).unwrap() == 'O' {
-            for above in (0..p.row).rev() {}
+            let destination = roll_north(&rocks, p);
+            rocks.swap(p, destination);
         }
     }
 }
 
 fn roll_north(rocks: &GridCharWorld, p: Position) -> Position {
+    let mut prev = p;
     for above in (0..p.row).rev() {
         let candidate = Position {
             col: p.col,
             row: above,
         };
-        if rocks.value(candidate).unwrap() != '.' {}
+        if rocks.value(candidate).unwrap() != '.' {
+            return prev;
+        }
+        prev = candidate;
     }
     Position { col: p.col, row: 0 }
 }
