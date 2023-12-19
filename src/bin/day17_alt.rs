a7443d04778d1ac64288cb2292aa8022fa0d58fb
gjf2a <ferrer@hendrix.edu>
2023-12-19 15:25:14 -0600

    Progress on alternative A* for Day 17.

--- src/bin/day17_alt.rs
+++ src/bin/day17_alt.rs
@@ -1,9 +1,7 @@
 use advent_code_lib::{
-    chooser_main, DirType, GridCharWorld, GridDigitWorld, ManhattanDir, Part, Position,
+    chooser_main, DirType, GridCharWorld, GridDigitWorld, ManhattanDir, Part, Position, heuristic_search,
 };
 use bare_metal_modulo::MNum;
-use enum_iterator::all;
-use indexmap::{IndexMap, IndexSet};
 
 fn main() -> anyhow::Result<()> {
     chooser_main(|filename, part| {
@@ -13,108 +11,64 @@ fn main() -> anyhow::Result<()> {
             heat_loss_map.height(),
             heat_loss_map.width()
         );
-        let mut table = CrucibleCostTable::new(
-            &heat_loss_map,
-            Position { row: 0, col: 0 },
-            Position {
-                row: heat_loss_map.height() as isize - 1,
-                col: heat_loss_map.width() as isize - 1,
-            },
-        );
-        let mut i = 1;
-        while !table.finished() {
-            match part {
-                Part::One => table.next_level(1, 3),
-                Part::Two => table.next_level(4, 10),
-            };
-            println!("Level: {} ({})", i, table.pending.len());
-            if let Some((cost, _)) = &table.solution {
-                println!("Cost: {cost}");
-            }
-            i += 1;
-        }
-        let (best_cost, best_path) = table.solution.unwrap();
-        for p in best_path.iter() {
-            print!("{p} ");
-        }
-        println!();
-        println!("Heat loss: {best_cost}");
-        Ok(())
-    })
-}
-
-#[derive(Debug)]
-struct CrucibleCostTable {
-    pending: IndexMap<(Position, usize, ManhattanDir), u64>,
-    solution: Option<(u64, IndexSet<Position>)>,
-    heat_loss_map: GridDigitWorld,
-    goal: Position,
-}
-
-impl CrucibleCostTable {
-    fn new(heat_loss_map: &GridDigitWorld, start: Position, goal: Position) -> Self {
-        let mut one = IndexMap::new();
-        for dir in all::<ManhattanDir>() {
-            let neighbor = dir.next_position(start);
-            if let Some(loss) = heat_loss_map.value(neighbor) {
-                let mut prev = IndexSet::new();
-                prev.insert(start);
-                prev.insert(neighbor);
-                one.insert((neighbor, 1, dir), (loss.a() as u64, vec![dir], prev));
-            }
-        }
-        Self {
-            solution: None,
-            pending: one,
-            heat_loss_map: heat_loss_map.clone(),
-            goal,
-        }
-    }
-
-    fn next_level(&mut self, streak_min: usize, streak_max: usize) {
-        let mut level: IndexMap<
-            (Position, usize, ManhattanDir),
-            (u64, Vec<ManhattanDir>, IndexSet<Position>),
-        > = IndexMap::new();
-        for ((pos, _, last_dir), (cost, dirs, path)) in self.pending.iter() {
-            for dir in [*last_dir, last_dir.clockwise(), last_dir.counterclockwise()] {
-                let streak = 1 + dirs.iter().rev().take_while(|d| **d == dir).count();
-                let prev_streak = dirs.iter().rev().take_while(|d| *d == last_dir).count();
-                if (dir == *last_dir || prev_streak >= streak_min) && streak <= streak_max {
-                    let neighbor = dir.next_position(*pos);
-                    if !path.contains(&neighbor) {
-                        if let Some(loss) = self.heat_loss_map.value(neighbor) {
-                            let mut new_path = path.clone();
-                            new_path.insert(neighbor);
-                            let mut new_dirs = dirs.clone();
-                            new_dirs.push(dir);
-                            let neighbor_cost = *cost + loss.a() as u64;
-                            if neighbor == self.goal {
-                                let goal_cost =
-                                    self.solution.as_ref().map_or(u64::MAX, |(c, _)| *c);
-                                if streak >= streak_min && neighbor_cost < goal_cost {
-                                    self.solution = Some((neighbor_cost, new_path));
-                                }
-                            } else {
-                                let better = level
-                                    .get(&(neighbor, streak, dir))
-                                    .map_or(true, |(other_cost, _, _)| neighbor_cost < *other_cost);
-                                if better {
-                                    level.insert(
-                                        (neighbor, streak, dir),
-                                        (neighbor_cost, new_dirs, new_path),
-                                    );
-                                }
-                            }
+        let (streak_min, streak_max) = match part {
+            Part::One => (1, 3),
+            Part::Two => (4, 10),
+        };
+        let goal = Position {row: heat_loss_map.height() as isize - 1, col: heat_loss_map.width() as isize - 1 };
+        let result = heuristic_search(CrucibleStatus::default(), |c| c.total_heat_loss, |c| c.p == goal, |c| c.p.manhattan_distance(goal) as u64, |c, p| {
+            let mut result = vec![];
+            for dir in [c.incoming, c.incoming.clockwise(), c.incoming.counterclockwise()] {
+                if dir != c.incoming || c.streak < streak_max && dir == c.incoming || c.streak >= streak_min {
+                    let neighbor = dir.next_position(c.p);
+                    let path_back = p.path_back_from(c);
+                    if path_back.map_or(true, |path| path.iter().all(|pc| pc.p != neighbor)) {
+                        if let Some(loss) = heat_loss_map.value(neighbor) {
+                            let total_heat_loss = c.total_heat_loss + loss.a() as u64;
+                            let streak = if dir == c.incoming {c.streak + 1} else {1};
+                            result.push(CrucibleStatus { p: neighbor, streak, incoming: dir, total_heat_loss });
                         }
                     }
                 }
             }
-        }
-        std::mem::swap(&mut self.pending, &mut level);
-    }
+            result
+        });
+        let total_heat_loss = result.node_at_goal().unwrap().total_heat_loss;
+        let path_back = result.path().unwrap();
+        visualize(filename, path_back.iter().map(|c| c.p))?;
+        println!("Part {part:?}: {total_heat_loss}");
+        
+        Ok(())
+    })
+}
 
-    fn finished(&self) -> bool {
-        self.pending.len() == 0
+#[derive(Clone, Eq, PartialEq, Debug, Hash)]
+struct CrucibleStatus {
+    p: Position,
+    streak: usize,
+    incoming: ManhattanDir,
+    total_heat_loss: u64,
+}
+
+impl Default for CrucibleStatus {
+    fn default() -> Self {
+        Self { p: Default::default(), streak: 0, incoming: ManhattanDir::E, total_heat_loss: 0 }
     }
+}
+
+fn visualize(filename: &str, mut path: impl Iterator<Item=Position>) -> anyhow::Result<()> {
+    let mut chars = GridCharWorld::from_char_file(filename)?;
+    let mut prev = path.next().unwrap();
+    for next in path {
+        let c = match prev.manhattan_dir_to(next).unwrap() {
+            ManhattanDir::N => '^',
+            ManhattanDir::E => '>',
+            ManhattanDir::S => 'v',
+            ManhattanDir::W => '<',
+        };
+        chars.modify(next, |v| *v = c);
+        prev = next;
+    }
+    println!("{chars}");
+    Ok(())
 }
