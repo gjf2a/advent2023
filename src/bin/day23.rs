77faad2d3fcb046304edcc35181b803e49bd0e67
gjf2a <ferrer@hendrix.edu>
2023-12-23 10:54:14 -0600

    Junction graph works.

--- src/bin/day23.rs
+++ src/bin/day23.rs
@@ -1,7 +1,21 @@
 use advent_code_lib::{chooser_main, DirType, GridCharWorld, ManhattanDir, Part, Position};
+use bare_metal_modulo::{ModNum, MNum};
 use enum_iterator::all;
 use im::Vector;
-use indexmap::IndexSet;
+use indexmap::{IndexSet, IndexMap};
+
+// Part 2 is probably somewhere slightly higher than 4750, based on a different buggy solution. 
+// * 4750 is definitely too low.
+// * That took 15 minutes with the buggy solution.
+
+const START: Position = Position {row: 0, col: 1};
+
+fn goal(map: &GridCharWorld) -> Position {
+    Position {
+        row: map.height() as isize - 1,
+        col: map.width() as isize - 2,
+    }
+}
 
 fn main() -> anyhow::Result<()> {
     chooser_main(|filename, part, options| {
@@ -27,6 +41,17 @@ fn main() -> anyhow::Result<()> {
                             }
                             println!();
                         }
+                        "-j" => {
+                            let map = GridCharWorld::from_char_file(filename)?;
+                            let junctions = JunctionDistances::new(&map);
+                            for (node, edge) in junctions.junctions2neighbors.iter() {
+                                print!("{node}:");
+                                for (target, weight) in edge.iter() {
+                                    print!(" {target}:{weight} ");
+                                }
+                                println!();
+                            }
+                        }
                         _ => println!("Unrecognized option"),
                     }
                 } else {
@@ -39,41 +64,61 @@ fn main() -> anyhow::Result<()> {
     })
 }
 
-fn goal(map: &GridCharWorld) -> Position {
-    Position {
-        row: map.height() as isize - 1,
-        col: map.width() as isize - 2,
-    }
+#[derive(Debug)]
+struct JunctionDistances {
+    junctions2neighbors: IndexMap<Position,IndexMap<Position, usize>>
 }
 
-const CYCLE_CHAR: char = '@';
-
-struct CycleChecker {
-    visited: IndexSet<Position>,
-    map: GridCharWorld,
-}
-
-impl CycleChecker {
-    fn get_cycles(filename: &str) -> anyhow::Result<Self> {
-        let map = GridCharWorld::from_char_file(filename)?;
-        let mut checker = Self {map, visited: IndexSet::new()};
-        checker.find_cycle_members_from(Position {row: 0, col: 1}, None);
-        Ok(checker)
-    }
-
-    fn cycle_members(&self) -> impl Iterator<Item=Position> + '_ {
-        self.map.position_value_iter().filter(|(_,v)| **v == CYCLE_CHAR).map(|(p,_)| *p)
-    }
-
-    fn find_cycle_members_from(&mut self, p: Position, parent: Option<Position>) {
-        self.visited.insert(p);
-        for neighbor in p.manhattan_neighbors().filter(|n| parent.map_or(true, |pt| pt != *n)) {
-            if self.map.value(neighbor).map_or(false, |v| v != '#') {
-                if self.visited.contains(&neighbor) {
-                    self.map.modify(neighbor, |v| *v = CYCLE_CHAR);
-                } else {
-                    self.find_cycle_members_from(neighbor, Some(p));
+impl JunctionDistances {
+    fn new(map: &GridCharWorld) -> Self {
+        let goal = goal(map);
+        let mut open_list = Vector::new();
+        open_list.push_back((ManhattanDir::S.next_position(START), START, START, 1));
+        let mut junctions2neighbors = IndexMap::new();
+        junctions2neighbors.insert(START, IndexMap::new());
+        junctions2neighbors.insert(goal, IndexMap::new());
+        let mut visited = IndexSet::new();
+        while let Some((node, parent, last_junction, last_junction_distance)) = open_list.pop_front() {
+            if !visited.contains(&(node, parent, last_junction, last_junction_distance)) {
+                visited.insert((node, parent, last_junction, last_junction_distance));
+                let neighbors = node.manhattan_neighbors().filter(|n| *n != parent && map.value(*n).map_or(false, |v| v != '#')).collect::<Vec<_>>();
+                if neighbors.contains(&goal) {
+                    update_both(last_junction_distance + 1, last_junction, goal, &mut junctions2neighbors);
+                } else if neighbors.len() == 1 {
+                    open_list.push_back((neighbors[0], node, last_junction, last_junction_distance + 1));
+                } else if neighbors.len() > 1 {
+                    update_both(last_junction_distance, last_junction, node, &mut junctions2neighbors);
+                    for neighbor in neighbors {
+                        open_list.push_back((neighbor, node, node, 1));
+                    }
                 }
+            }
+        }
+        Self {junctions2neighbors}
+    }
+}
+
+fn update_both(distance: usize, p1: Position, p2: Position, junctions2neighbors: &mut IndexMap<Position,IndexMap<Position,usize>>) {
+    let ps = [p1, p2];
+    for p in ps.iter() {
+        if !junctions2neighbors.contains_key(p) {
+            junctions2neighbors.insert(*p, IndexMap::new());
+        }
+    }
+
+    for i in ModNum::new(0, ps.len()).iter() {
+        update_bigger(distance, ps[i.a()], junctions2neighbors.get_mut(&ps[(i + 1).a()]).unwrap());
+    }
+}
+
+fn update_bigger(distance: usize, source: Position, target_table: &mut IndexMap<Position, usize>) {
+    match target_table.get_mut(&source) {
+        None => {
+            target_table.insert(source, distance);
+        }
+        Some(old_distance) => {
+            if distance > *old_distance {
+                *old_distance = distance;
             }
         }
     }
@@ -81,8 +126,7 @@ impl CycleChecker {
 
 struct LongPathTable {
     map: GridCharWorld,
-    destinations_at_length: Vec<Vector<Position>>,
-    paths_at_length: Vec<Vec<Vector<Position>>>,
+    paths_of_length: Vec<IndexSet<(Position, Vector<Position>)>>,
     expanding: bool,
     goal: Position,
     hike_up_slope: bool,
@@ -91,66 +135,52 @@ struct LongPathTable {
 impl LongPathTable {
     fn new(filename: &str, hike_up_slope: bool) -> anyhow::Result<Self> {
         let map = GridCharWorld::from_char_file(filename)?;
-        let mut level_0 = Vector::new();
-        level_0.push_back(Position {row: 0, col: 1});
-        let destinations_at_length = vec![level_0];
-        let paths_at_length = vec![vec![Vector::new()]];
+        let mut start_set = IndexSet::new();
+        start_set.insert((START, Vector::new()));
+        let paths_of_length = vec![start_set];
         let goal = goal(&map);
         Ok(Self {
             map,
-            destinations_at_length,
-            paths_at_length,
+            paths_of_length,
             expanding: true,
             goal,
             hike_up_slope,
         })
     }
 
-    fn last_level(&self) -> usize {
-        assert_eq!(self.destinations_at_length.len(), self.paths_at_length.len());
-        self.destinations_at_length.len() - 1
-    }
-
     fn expand(&mut self) {
         let mut expanding = false;
-        let mut new_destinations = Vector::new();
-        let mut new_paths = vec![];
-        for i in 0..self.destinations_at_length[self.last_level()].len() {
-            let candidate = self.destinations_at_length[self.last_level()][i];
-            if candidate != self.goal {
-                let path = &self.paths_at_length[self.last_level()][i];
-                for neighbor in self.traversible_neighbors(&candidate, path) {
-                    if !new_destinations.contains(&neighbor) {
-                        new_destinations.push_back(neighbor);
-                        let mut new_path = path.clone();
-                        new_path.push_back(candidate);
-                        new_paths.push(new_path);
-                        expanding = true;
-                    }
+        let mut new_level = IndexSet::new();
+        for (candidate, path) in self.paths_of_length[self.paths_of_length.len() - 1].iter() {
+            if *candidate != self.goal {
+                for neighbor in self.traversible_neighbors(candidate, path) {
+                    let mut new_path = path.clone();
+                    new_path.push_back(*candidate);
+                    new_level.insert((neighbor, new_path));
+                    expanding = true;
                 }
             }
         }
         self.expanding = expanding;
-        self.destinations_at_length.push(new_destinations);
-        self.paths_at_length.push(new_paths);
+        self.paths_of_length.push(new_level);
     }
 
     fn expand_fully(&mut self, show_levels: bool) {
         while self.expanding {
             self.expand();
             if show_levels {
-                println!("Finished level {} (/{}) ({} nodes)", self.paths_at_length.len(), self.map.width() * self.map.height(), self.paths_at_length[self.last_level()].len());
+                println!("Finished level {} (/{}) ({} nodes)", self.paths_of_length.len(), self.map.width() * self.map.height(), self.paths_of_length.last().unwrap().len());
             }
         }
     }
 
     fn max_goal_level(&self) -> usize {
-        (0..self.destinations_at_length.len())
+        (0..self.paths_of_length.len())
             .rev()
             .find(|i| {
-                self.destinations_at_length[*i]
+                self.paths_of_length[*i]
                     .iter()
-                    .any(|p| *p == self.goal)
+                    .any(|(p, _)| *p == self.goal)
             })
             .unwrap()
     }
@@ -174,5 +204,38 @@ impl LongPathTable {
             })
             .map(|d| d.next_position(*p))
             .filter(|n| !path.contains(n) && self.map.value(*n).map_or(false, |v| v != '#'))
+    }
+}
+
+const CYCLE_CHAR: char = '@';
+
+struct CycleChecker {
+    visited: IndexSet<Position>,
+    map: GridCharWorld,
+}
+
+impl CycleChecker {
+    fn get_cycles(filename: &str) -> anyhow::Result<Self> {
+        let map = GridCharWorld::from_char_file(filename)?;
+        let mut checker = Self {map, visited: IndexSet::new()};
+        checker.find_cycle_members_from(START, None);
+        Ok(checker)
+    }
+
+    fn cycle_members(&self) -> impl Iterator<Item=Position> + '_ {
+        self.map.position_value_iter().filter(|(_,v)| **v == CYCLE_CHAR).map(|(p,_)| *p)
+    }
+
+    fn find_cycle_members_from(&mut self, p: Position, parent: Option<Position>) {
+        self.visited.insert(p);
+        for neighbor in p.manhattan_neighbors().filter(|n| parent.map_or(true, |pt| pt != *n)) {
+            if self.map.value(neighbor).map_or(false, |v| v != '#') {
+                if self.visited.contains(&neighbor) {
+                    self.map.modify(neighbor, |v| *v = CYCLE_CHAR);
+                } else {
+                    self.find_cycle_members_from(neighbor, Some(p));
+                }
+            }
+        }
     }
 }
