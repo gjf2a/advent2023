cdf1302c1f182e1f489c8bee49f087c532ee80a6
gjf2a <ferrer@hendrix.edu>
2023-12-24 05:12:45 -0600

    The counting version doesn't really work - if I don't add, it only counts immediate neighboring sectors - if I do add, it overcounts greatly. It doesn't seem to have enough information to distinguish them.

--- src/bin/day21.rs
+++ src/bin/day21.rs
@@ -1,3 +1,5 @@
+use std::cmp::max;
+
 use advent_code_lib::{
     chooser_main, GridCharWorld, Part, Position, ManhattanDir, DirType,
 };
@@ -19,11 +21,19 @@ fn main() -> anyhow::Result<()> {
         match part {
             Part::One => {
                 let iterations = if filename.contains("ex") { 6 } else { 64 };
-                let mut table = UnboundedTable::new(&garden, false);
-                for _ in 0..iterations {
-                    table.expand_once();
+                if options.len() > 0 {
+                    let mut table = CountingTable::new(&garden, false);
+                    for _ in 0..iterations {
+                        table.expand_once();
+                    }
+                    println!("Part {part:?}: {}", table.current_reachable());
+                } else {
+                    let mut table = UnboundedTable::new(&garden, false);
+                    for _ in 0..iterations {
+                        table.expand_once();
+                    }
+                    println!("Part {part:?}: {}", table.current_reachable());
                 }
-                println!("Part {part:?}: {}", table.current_reachable());
             }
             Part::Two => {
                 let iterations = if options.len() > 0 {
@@ -31,16 +41,88 @@ fn main() -> anyhow::Result<()> {
                 } else {
                     26501365
                 };
-                let mut table = UnboundedTable::new(&garden, true);
-                for _ in 0..iterations {
-                    table.expand_once();
+                if options.len() > 1 {
+                    let mut table = CountingTable::new(&garden, true);
+                    for _ in 0..iterations {
+                        table.expand_once();
+                    }
+                    println!("Part {part:?}: {}", table.current_reachable());
+                } else {
+                    let mut table = UnboundedTable::new(&garden, true);
+                    for _ in 0..iterations {
+                        table.expand_once();
+                    }
+                    println!("Part {part:?}: {}", table.current_reachable());
                 }
-                println!("Part {part:?}: {}", table.current_reachable());
             }
         }
 
         Ok(())
     })
+}
+
+fn bounded(p: Position, garden: &GridCharWorld) -> Position {
+    Position {
+        row: p.row.mod_floor(&(garden.height() as isize)),
+        col: p.col.mod_floor(&(garden.width() as isize)),
+    }
+}
+
+struct CountingTable {
+    garden: GridCharWorld,
+    table: [IndexMap<Position,u128>; 2],
+    current: ModNumC<usize, 2>,
+    wrap: bool,
+}
+
+impl CountingTable {
+    fn new(garden: &GridCharWorld, wrap: bool) -> Self {
+        let mut table = [IndexMap::new(), IndexMap::new()];
+        for i in 0..table.len() {
+            for (p, v) in garden.position_value_iter().filter(|(_,v)| **v != '#') {
+                table[i].insert(*p, 0);
+                if *v == 'S' && i == 0 {
+                    *table[0].get_mut(p).unwrap() = 1;
+                }
+            }
+        }
+        Self {garden: garden.clone(), table: table, current: ModNumC::new(0), wrap}
+    }
+
+    fn current_reachable(&self) -> u128 {
+        self.table[self.current.a()].iter().map(|(_,sources)| sources).sum()
+    }
+
+    fn expand_once(&mut self) {
+        let source = self.current.a();
+        let target = (self.current + 1).a();
+        let mut updates = self.table[target].keys().map(|k| (*k, 0)).collect::<IndexMap<_,_>>();
+        for p in self.table[target].keys() {
+            for dir in all::<ManhattanDir>() {
+                let neighbor = dir.next_position(*p);
+                match self.garden.value(neighbor) {
+                    Some(v) => {
+                        if v != '#' {
+                            let current = updates.get(p).copied().unwrap();
+                            *updates.get_mut(p).unwrap() = max(current, self.table[source].get(&neighbor).copied().unwrap_or(0));
+                        }
+                    }
+                    None => {
+                        let neighbor = bounded(neighbor, &self.garden);
+                        let v = self.garden.value(neighbor).unwrap();
+                        if self.wrap && v != '#' {
+                            let current = updates.get(p).copied().unwrap();// + 1;
+                            *updates.get_mut(p).unwrap() = max(current, self.table[source].get(&neighbor).copied().unwrap_or(0));
+                        }
+                    }
+                }
+            }
+        }
+        for (p, sources) in updates {
+            *self.table[target].get_mut(&p).unwrap() = sources;
+        }
+        self.current += 1;
+    }
 }
 
 struct UnboundedTable {
@@ -105,12 +187,5 @@ impl UnboundedTable {
             }
         }
         self.current += 1;
-    }
-}
-
-fn bounded(p: Position, garden: &GridCharWorld) -> Position {
-    Position {
-        row: p.row.mod_floor(&(garden.height() as isize)),
-        col: p.col.mod_floor(&(garden.width() as isize)),
     }
 }
