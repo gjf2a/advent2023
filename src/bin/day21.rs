0e04a16213736baaa7451d48f0cfe1d562816d8e
gjf2a <ferrer@hendrix.edu>
2023-12-24 10:36:55 -0600

    Started Day 24.

--- src/bin/day21.rs
+++ src/bin/day21.rs
@@ -1,13 +1,11 @@
 use std::cmp::max;
 
-use advent_code_lib::{
-    chooser_main, GridCharWorld, Part, Position, ManhattanDir, DirType,
-};
+use advent_code_lib::{chooser_main, DirType, GridCharWorld, ManhattanDir, Part, Position};
 use bare_metal_modulo::{MNum, ModNumC};
 use enum_iterator::all;
+use im::OrdSet;
 use indexmap::IndexMap;
 use num_integer::Integer;
-use im::OrdSet;
 
 /*
 Example alternates between 39 and 42 active starting at step 13.
@@ -17,7 +15,7 @@ Input alternates between 7255 and 7262 active starting at 129.
 fn main() -> anyhow::Result<()> {
     chooser_main(|filename, part, options| {
         let garden = GridCharWorld::from_char_file(filename)?;
-        
+
         match part {
             Part::One => {
                 let iterations = if filename.contains("ex") { 6 } else { 64 };
@@ -70,7 +68,7 @@ fn bounded(p: Position, garden: &GridCharWorld) -> Position {
 
 struct CountingTable {
     garden: GridCharWorld,
-    table: [IndexMap<Position,u128>; 2],
+    table: [IndexMap<Position, u128>; 2],
     current: ModNumC<usize, 2>,
     wrap: bool,
 }
@@ -79,24 +77,35 @@ impl CountingTable {
     fn new(garden: &GridCharWorld, wrap: bool) -> Self {
         let mut table = [IndexMap::new(), IndexMap::new()];
         for i in 0..table.len() {
-            for (p, v) in garden.position_value_iter().filter(|(_,v)| **v != '#') {
+            for (p, v) in garden.position_value_iter().filter(|(_, v)| **v != '#') {
                 table[i].insert(*p, 0);
                 if *v == 'S' && i == 0 {
                     *table[0].get_mut(p).unwrap() = 1;
                 }
             }
         }
-        Self {garden: garden.clone(), table: table, current: ModNumC::new(0), wrap}
+        Self {
+            garden: garden.clone(),
+            table: table,
+            current: ModNumC::new(0),
+            wrap,
+        }
     }
 
     fn current_reachable(&self) -> u128 {
-        self.table[self.current.a()].iter().map(|(_,sources)| sources).sum()
+        self.table[self.current.a()]
+            .iter()
+            .map(|(_, sources)| sources)
+            .sum()
     }
 
     fn expand_once(&mut self) {
         let source = self.current.a();
         let target = (self.current + 1).a();
-        let mut updates = self.table[target].keys().map(|k| (*k, 0)).collect::<IndexMap<_,_>>();
+        let mut updates = self.table[target]
+            .keys()
+            .map(|k| (*k, 0))
+            .collect::<IndexMap<_, _>>();
         for p in self.table[target].keys() {
             for dir in all::<ManhattanDir>() {
                 let neighbor = dir.next_position(*p);
@@ -104,15 +113,21 @@ impl CountingTable {
                     Some(v) => {
                         if v != '#' {
                             let current = updates.get(p).copied().unwrap();
-                            *updates.get_mut(p).unwrap() = max(current, self.table[source].get(&neighbor).copied().unwrap_or(0));
+                            *updates.get_mut(p).unwrap() = max(
+                                current,
+                                self.table[source].get(&neighbor).copied().unwrap_or(0),
+                            );
                         }
                     }
                     None => {
                         let neighbor = bounded(neighbor, &self.garden);
                         let v = self.garden.value(neighbor).unwrap();
                         if self.wrap && v != '#' {
-                            let current = updates.get(p).copied().unwrap();// + 1;
-                            *updates.get_mut(p).unwrap() = max(current, self.table[source].get(&neighbor).copied().unwrap_or(0));
+                            let current = updates.get(p).copied().unwrap(); // + 1;
+                            *updates.get_mut(p).unwrap() = max(
+                                current,
+                                self.table[source].get(&neighbor).copied().unwrap_or(0),
+                            );
                         }
                     }
                 }
@@ -127,7 +142,7 @@ impl CountingTable {
 
 struct UnboundedTable {
     garden: GridCharWorld,
-    table: [IndexMap<Position,OrdSet<Position>>; 2],
+    table: [IndexMap<Position, OrdSet<Position>>; 2],
     current: ModNumC<usize, 2>,
     wrap: bool,
 }
@@ -136,24 +151,38 @@ impl UnboundedTable {
     fn new(garden: &GridCharWorld, wrap: bool) -> Self {
         let mut table = [IndexMap::new(), IndexMap::new()];
         for i in 0..table.len() {
-            for (p, v) in garden.position_value_iter().filter(|(_,v)| **v != '#') {
+            for (p, v) in garden.position_value_iter().filter(|(_, v)| **v != '#') {
                 table[i].insert(*p, OrdSet::new());
                 if *v == 'S' && i == 0 {
-                    table[0].get_mut(p).unwrap().insert(Position {row: 0, col: 0});
+                    table[0]
+                        .get_mut(p)
+                        .unwrap()
+                        .insert(Position { row: 0, col: 0 });
                 }
             }
         }
-        Self {garden: garden.clone(), table: table, current: ModNumC::new(0), wrap}
+        Self {
+            garden: garden.clone(),
+            table: table,
+            current: ModNumC::new(0),
+            wrap,
+        }
     }
 
     fn current_reachable(&self) -> u128 {
-        self.table[self.current.a()].iter().map(|(_,sources)| sources.len() as u128).sum()
+        self.table[self.current.a()]
+            .iter()
+            .map(|(_, sources)| sources.len() as u128)
+            .sum()
     }
 
     fn expand_once(&mut self) {
         let source = self.current.a();
         let target = (self.current + 1).a();
-        let mut insertions = self.table[target].keys().map(|k| (*k, OrdSet::<Position>::new())).collect::<IndexMap<_,_>>();
+        let mut insertions = self.table[target]
+            .keys()
+            .map(|k| (*k, OrdSet::<Position>::new()))
+            .collect::<IndexMap<_, _>>();
         for p in self.table[target].keys() {
             for dir in all::<ManhattanDir>() {
                 let neighbor = dir.next_position(*p);
@@ -173,7 +202,10 @@ impl UnboundedTable {
                         if self.wrap && v != '#' {
                             if let Some(sources) = self.table[source].get(&neighbor) {
                                 for src in sources.iter() {
-                                    insertions.get_mut(p).unwrap().insert(dir.inverse().next_position(*src));
+                                    insertions
+                                        .get_mut(p)
+                                        .unwrap()
+                                        .insert(dir.inverse().next_position(*src));
                                 }
                             }
                         }
---
+++ src/bin/day24.rs
@@ -0,0 +1,146 @@
+use std::fmt::Display;
+
+use advent_code_lib::{all_lines, chooser_main, Part, Point};
+
+fn main() -> anyhow::Result<()> {
+    chooser_main(|filename, part, options| {
+        let points = all_lines(filename)?
+            .map(|line| {
+                let mut parts = line.split('@');
+                let pos = parts
+                    .next()
+                    .unwrap()
+                    .trim()
+                    .parse::<Point<i128, 3>>()
+                    .unwrap();
+                let vec = parts
+                    .next()
+                    .unwrap()
+                    .trim()
+                    .parse::<Point<i128, 3>>()
+                    .unwrap();
+                (pos, vec)
+            })
+            .collect::<Vec<_>>();
+
+        match part {
+            Part::One => {
+                let points = points
+                    .iter()
+                    .map(|(p, v)| {
+                        (
+                            Point::<i128, 2>::from_iter(p.values().take(2)),
+                            Point::<i128, 2>::from_iter(v.values().take(2)),
+                        )
+                    })
+                    .collect::<Vec<_>>();
+                if options.len() > 0 {
+                    if options.contains(&"-input".to_owned()) {
+                        for point in points.iter() {
+                            println!("{} @ {}", point.0, point.1);
+                        }
+                    }
+                    if options.contains(&"-intersections".to_owned()) {
+                        for (i, (p1, d1)) in points.iter().enumerate() {
+                            let line1 = Line2D::new(&points[i]);
+                            for j in (i + 1)..points.len() {
+                                let line2 = Line2D::new(&points[j]);
+                                println!("A: {p1} @ {d1} ({line1})");
+                                println!("B: {} @ {} ({line2})", points[j].0, points[j].1);
+                                let intersection = line1.intersection(&line2);
+                                println!("{intersection:?}\n");
+                            }
+                        }
+                    }
+                    if options.contains(&"-future".to_owned()) {
+                        for (i, (p1, d1)) in points.iter().enumerate() {
+                            for j in (i + 1)..points.len() {
+                                println!("A: {p1} @ {d1}");
+                                println!("B: {} @ {}", points[j].0, points[j].1);
+                                let intersection = future_intersection(&(*p1, *d1), &points[j]);
+                                println!("{intersection:?}\n");
+                            }
+                        }
+                    }
+                }
+            }
+            Part::Two => {
+                if options.len() > 0 && options.contains(&"-input".to_owned()) {
+                    for point in points.iter() {
+                        println!("{} @ {}", point.0, point.1);
+                    }
+                }
+            }
+        }
+        Ok(())
+    })
+}
+
+type Hailstone2d = (Point<i128, 2>, Point<i128, 2>);
+
+fn within(intersection: Option<(f64, f64)>, min: i128, max: i128) -> Option<(f64, f64)> {
+    let min = min as f64;
+    let max = max as f64;
+    intersection.filter(|(x, y)| min <= *x && *x <= max && min <= *y && *y <= max)
+}
+
+fn future_intersection(a: &Hailstone2d, b: &Hailstone2d) -> Option<(f64, f64)> {
+    let line1 = Line2D::new(a);
+    let line2 = Line2D::new(b);
+    line1
+        .intersection(&line2)
+        .filter(|(_, y)| in_future(a, *y) && in_future(b, *y))
+}
+
+fn in_future(stone: &Hailstone2d, y: f64) -> bool {
+    let (point, delta) = stone;
+    y > point[1] as f64 && delta[1] > 0 || y < point[1] as f64 && delta[1] < 0
+}
+
+// y = (rise/run)x + b
+// -rise/run x + y = b
+// -rise x + run y = run b
+struct Line2D {
+    slope: f64,
+    y_intercept: f64,
+    a: i128,
+    b: i128,
+    c: f64,
+}
+
+impl Line2D {
+    fn new(stone: &Hailstone2d) -> Self {
+        let (point, delta) = stone;
+        let rise = delta[1];
+        let run = delta[0];
+        let slope = rise as f64 / run as f64;
+        let y_intercept = point[1] as f64 - slope * point[0] as f64;
+        let a = -rise;
+        let b = run;
+        let c = y_intercept * run as f64;
+        Self {
+            slope,
+            y_intercept,
+            a,
+            b,
+            c,
+        }
+    }
+
+    fn intersection(&self, other: &Self) -> Option<(f64, f64)> {
+        if self.slope != other.slope {
+            let x = (self.c * other.b as f64 - self.b as f64 * other.c)
+                / (self.a * other.b - self.b * other.a) as f64;
+            let y = self.slope * x + self.y_intercept;
+            Some((x, y))
+        } else {
+            None
+        }
+    }
+}
+
+impl Display for Line2D {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} x + {} y = {}", self.a, self.b, self.c)
+    }
+}
